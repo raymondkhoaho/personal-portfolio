e420c8f1ceaa954f634f99db63f28507b4d321b9
Raymond Ho <raymondkhoaho@gmail.com>
2023-03-27 22:00:41 +0000

    modified page structure

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-// import AppContext from '../lib/app-context';
 
 export default class NavBar extends React.Component {
   constructor(props) {
@@ -12,27 +8,7 @@ export default class NavBar extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar collapseOnSelect className="navbar-top" fixed="top" bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand className="logo fs-2" href="#explore">InstaPet</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto d-lg-flex align-items-center">
-                <Nav.Link href="#explore"><i className="fa-solid fa-magnifying-glass p-2" />Explore</Nav.Link>
-                <Nav.Link href="#users"><i className="icon fa-solid fa-users p-2" />Users</Nav.Link>
-                <Nav.Link href="#newpost"><i className="fa-solid fa-circle-plus p-2" />New Post</Nav.Link>
-                <NavDropdown id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
-                  <NavDropdown.Item onClick={this.context.handleSignOut} href="">Sign out</NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </div>
+      <div>NavBar</div>
     );
   }
 }
-
-// NavBar.contextType = AppContext;
