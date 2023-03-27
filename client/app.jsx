388dd79718753c565873279590a02a99685d7d8d
Raymond Ho <raymondkhoaho@gmail.com>
2023-03-27 21:58:28 +0000

    added page structure

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import NavBar from './components/navbar';
+import About from './components/about-me';
+import Contact from './components/contact';
+import Projects from './components/projects';
+import Technologies from './components/technologies';
 
 export default class App extends React.Component {
   render() {
-    return <NavBar />;
+    return (
+      <main>
+        <NavBar />
+        <About />
+        <Projects />
+        <Technologies />
+        <Contact />
+      </main>
+    );
   }
 }
---
+++ client/components/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+
+  render() {
+    return (
+      <div>Contact Me</div>
+    );
+  }
+}
