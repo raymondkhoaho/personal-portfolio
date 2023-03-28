84805ba0d945fb825b6cd08ee68c0f23c284d75a
Raymond Ho <raymondkhoaho@gmail.com>
2023-03-28 22:37:26 +0000

    added logo images to folder

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -12,7 +12,7 @@ export default class NavBar extends React.Component {
 
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">Raymond Ho</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
