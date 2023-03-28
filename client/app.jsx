ae59c63b1bad55a148c42fed03576f01a976d52a
Raymond Ho <raymondkhoaho@gmail.com>
2023-03-28 21:43:15 +0000

    added navbar skeleton and imported bootstrap css

--- client/app.jsx
+++ client/app.jsx
@@ -4,6 +4,7 @@ import About from './components/about-me';
 import Contact from './components/contact';
 import Projects from './components/projects';
 import Technologies from './components/technologies';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends React.Component {
   render() {
