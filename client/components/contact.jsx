5e9322455fa19e0cc4f2c7c6127e5708b825f2c9
Raymond Ho <raymondkhoaho@gmail.com>
2024-07-23 10:42:45 +0000

    mig class to func contacts

--- client/components/contact.jsx
+++ client/components/contact.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = null;
-  }
-
-  render() {
-    return (
-      <div>Contact Me</div>
-    );
-  }
+export default function Contact() {
+  return (
+    <div>Contact Me</div>
+  );
 }
