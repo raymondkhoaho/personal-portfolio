33c3f4d4ecb73b670ceaa5439d4c810e072a250a
Raymond Ho <raymondkhoaho@gmail.com>
2024-07-23 10:41:59 +0000

    mig class to func

--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 
-export default class Projects extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = null;
-  }
-
-  render() {
-    return (
-      <div>Projects</div>
-    );
-  }
+export default function Projects() {
+  return (
+    <div>Projects</div>
+  );
 }
