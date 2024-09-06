4e0e817e4af7b797e14fa54d20ca69c33885e3e1
Raymond Ho <raymondkhoaho@gmail.com>
2024-09-06 13:04:33 +0000

    social links

--- react-portfolio/src/components/SocialMediaIcons.jsx
+++ react-portfolio/src/components/SocialMediaIcons.jsx
@@ -1,38 +1,31 @@
 const SocialMediaIcons = () => {
   return (
-    <div className="flex justify-center md:justify-start my-10 gap-7">
+    <div className="flex justify-center md:justify-start my-10 gap-7 size-48">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com"
+        href="https://www.linkedin.com/in/raymondkhoaho"
         target="_blank"
         rel="noreferrer"
       >
-        <img alt="linkedin-link" src="../assets/linkedin.png" />
+        <img className="size-9" alt="linkedin-link" src="../assets/linkedin1.png" />
       </a>
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.twitter.com"
+        href="https://www.github.com/raymondkhoaho"
         target="_blank"
         rel="noreferrer"
       >
-        <img alt="twitter-link" src="../assets/twitter.png" />
+        <img className="size-9" alt="github-link" src="../assets/github-mark-white.png" />
       </a>
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.facebook.com"
+        href="mailto:raymondkhoaho@gmail.com"
         target="_blank"
         rel="noreferrer"
       >
-        <img alt="facebook-link" src="../assets/facebook.png" />
+        <img className="size-9" alt="email-link" src="../assets/email.png" />
       </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img alt="instagram-link" src="../assets/instagram.png" />
-      </a>
+
     </div>
   );
 };
